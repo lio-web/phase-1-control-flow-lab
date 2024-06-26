c68ef033c147c3b7f0e6abb3f400bcf904e87ec0
Leonard <leonard.muhati@student.moringaschool.com>
2024-06-26 18:28:17 +0300

    control lab

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){ 
+  if(distance <= '400'){
+    return 'This one is on me'
+  } else if(distance > '400'){
+    return 'This will be twenty bucks'
+  } else if(distance <='2000'){
+    return 'I will gladly take your thirty bucks'
+  } else if(distance > '2500'){
+    return 'No can do'
+  } 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ return city ==="NYC"?'Ok, sounds good.':city ==="Pittsburgh"`No go.`
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generous){
+  switch(generous){
+    case 'generouse':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+      default :
+        return 'Bye.'
+  }
+ 
 }
+
