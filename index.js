c4fb264fdffee44d94febd7dae0872d3c90058ab
Leonard <leonard.muhati@student.moringaschool.com>
2024-06-27 21:25:28 +0300

    my control lab

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-function scuberGreetingForFeet(distance){ 
-  if(distance <= '400'){
-    return 'This one is on me'
-  } else if(distance > '400'){
-    return 'This will be twenty bucks'
-  } else if(distance <='2000'){
-    return 'I will gladly take your thirty bucks'
-  } else if(distance > '2500'){
-    return 'No can do'
-  } 
-}
+function scuberGreetingForFeet(distance) {
 
+  if (distance <= 400){
+          return "This one is on me!";
+  }
+  else if (distance > 400 && distance < 2000){
+    return "That will be twenty bucks.";
+  }
+  else if (distance > 2000 && distance < 2500){
+      return "I will gladly take your thirty bucks.";
+  }
+  else if(distance > 2500){
+      return "No can do.";
+  }
+
+}
 function ternaryCheckCity(city){
- return city ==="NYC"?'Ok, sounds good.':city ==="Pittsburgh"`No go.`
+ return city ==="NYC"?'Ok, sounds good.':`No go.`
 }
-
 function switchOnCharmFromTip(generous){
   switch(generous){
-    case 'generouse':
+    case 'generous':
       return 'Thank you so much.'
     case 'not as generous':
       return 'Thank you.'
